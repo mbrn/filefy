2fb7ff8795a2273b71784e387cdc92172ea4a10f
Rob Beers <rbeers@blackhawkenterprise.com>
2019-03-11 09:42:05 -0400

    fixed spacing

--- src/core/BaseBuilder.ts
+++ src/core/BaseBuilder.ts
@@ -1,7 +1,7 @@
 export default class BaseBuilder {
   protected exportFile(dataType: string, fileName: string, data: string) {
-		var charBom = "\uFEFF";
-		var encodedData = encodeURIComponent(data);
+    var charBom = "\uFEFF";
+    var encodedData = encodeURIComponent(data);
     let content = `data:text/${dataType};charset=utf-8,${charBom}${encodedData}`;
 
     var link = document.createElement("a");
