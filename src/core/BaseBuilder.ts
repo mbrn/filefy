1fde35d4a4775d45aa3348e1440100382e58ab5c
Mehmet Baran <mehmetbaran@mehmetbaran.net>
2019-03-18 12:50:23 +0300

    Edge support added

--- src/core/BaseBuilder.ts
+++ src/core/BaseBuilder.ts
@@ -1,14 +1,22 @@
 export default class BaseBuilder {
   protected exportFile(dataType: string, fileName: string, data: string) {
-    var charBom = "\uFEFF";
-    var encodedData = encodeURIComponent(data);
-    let content = `data:text/${dataType};charset=utf-8,${charBom}${encodedData}`;
 
-    var link = document.createElement("a");
-    link.setAttribute("href", content);
-    link.setAttribute("download", fileName);
-    document.body.appendChild(link);
 
-    link.click();
+    if (window.navigator.msSaveOrOpenBlob) {
+      var blob = new Blob([data]);
+      window.navigator.msSaveOrOpenBlob(blob, fileName);
+    }
+    {
+      var charBom = "\uFEFF";
+      var encodedData = encodeURIComponent(data);
+      let content = `data:text/${dataType};charset=utf-8,${charBom}${encodedData}`;
+
+      var link = document.createElement("a");
+      link.setAttribute("href", content);
+      link.setAttribute("download", fileName);
+      document.body.appendChild(link);
+
+      link.click();
+    }
   }
 }
